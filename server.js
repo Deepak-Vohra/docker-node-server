5fe74901caf831e052f0bf481d64a09daa2e3ef8
dvohra <dvohra10@yahoo.com>
2016-11-28 12:11:06 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
-  response.end('Hello Jenkins World!');
+  response.end('Hello Jenkins!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
