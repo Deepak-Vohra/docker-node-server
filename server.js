9342ac9982a66983530a020cca8711a7481eb252
dvohra <dvohra10@yahoo.com>
2016-12-01 15:39:59 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
-  response.end('Hello Jenkins!');
+  response.end('Hello Jenkins Docker Plugin!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
