c567d52a4e8d3c619421b11413d2d1a0e74fe565
dvohra <dvohra10@yahoo.com>
2016-12-11 13:53:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello CodeCommit!');
+  response.end('Hello Jenkins!');
   
 };
 var www = http.createServer(handleRequest);
