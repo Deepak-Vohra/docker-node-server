e2a9ace370835025e407de94858788ff06566cf5
dvohra <dvohra10@yahoo.com>
2016-12-16 17:27:44 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Application!');
+  response.end('Hello from Node Application Deployed to Elastic Beanstalk!');
   
 };
 var www = http.createServer(handleRequest);
