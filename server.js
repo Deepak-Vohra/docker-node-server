e179217fc35ed99c355194d5a12326228c84a622
dvohra <dvohra10@yahoo.com>
2018-01-19 13:33:30 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello CodeCommit!');
+  response.end('Hello from Node Server App deployed on ECS Fargate!');
   
 };
 var www = http.createServer(handleRequest);
