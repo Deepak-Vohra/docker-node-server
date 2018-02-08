d8e83f8bff3290efc4aa94cb478947a903cb01c5
dvohra <dvohra10@yahoo.com>
2018-02-07 21:47:22 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate!');
+  response.end('Hello from Node Server App deployed on ECS Fargate with CodePipeline!');
   
 };
 var www = http.createServer(handleRequest);
