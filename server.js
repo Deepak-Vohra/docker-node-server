a3f428c377fab51fb8ad1bf5f3e20c508dae5da3
yahyagulshan <59036269+yahyagulshan@users.noreply.github.com>
2021-04-30 23:48:42 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate with CodePipeline!');
+  response.end('this is my new push!');
   
 };
 var www = http.createServer(handleRequest);
